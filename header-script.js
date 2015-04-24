1611c5552231b7184f3fe3a0ee55dda0d359d183
kelcel <keith.hargett@knights.ucf.edu>
2015-04-24 19:21:12 -0400

    better naming

--- header-script.js
+++ header-script.js
@@ -3,10 +3,19 @@ $(document).ready(function(){
     /*Toggle Secondary Nav*/
     $(".branding img").click(function(){
         $("#secondary").animate({left: "0px"} , "fast");
+
+        var width = parseInt($(window).width() *.06, 10) ;
+        var offset = parseInt($(".logo-wrap").css("left"), 10);
+
+        if (width == offset)
+            $(".logo-wrap").animate({left: "255px"}, "fast");
+        else
+            return;
     });
 
     $("#secondary .close").click(function(){
         $("#secondary").animate({left: "-250px"}, "fast");
+        $(".logo-wrap").animate({left: $(window).width()*.06}, "fast");
     });
     
     /*Menu Link Coloring*/
@@ -25,4 +34,8 @@ $(document).ready(function(){
             $("#menu > ul > li > a").css("color","#3B1F11");
         }
     );
+
+    $(".drop-center").parent().hover(function(){
+        $("#menu input").focus();
+    });
 });
