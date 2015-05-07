a0cb5de6e0c481c6ed35f63ba92405d1bbd9a66e
kelcel <keith.hargett@knights.ucf.edu>
2015-05-07 11:28:34 -0400

    fixed xhr looping

--- json.js
+++ json.js
@@ -25,10 +25,9 @@ function getData(url, xmlhttp)
     {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             var myArr = JSON.parse(xmlhttp.responseText);
-
             if (url == "features.json")
             {
-                displayGallery(myArr);
+                displayGallery(myArr[0]);
                 displayFeatures(myArr);
             }
             else if (url == "articles.json")
@@ -41,15 +40,15 @@ function getData(url, xmlhttp)
             }
        }
     }
-    
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
 }
 
 function displayGallery(arr)
 {
-    $("#gallery .title").text(arr[0].title);
-    $("#gallery .img").css("background-image","url(img/" + arr[0].img + ")");
+    $("#gallery .title").text(arr.title);
+    $("#gallery .img").css("background-image","url(img/" + arr.img + ")");
+    $("#gallery p").text(arr.descript);
 }
 
 function displayFeatures(arr)
@@ -62,7 +61,6 @@ function displayFeatures(arr)
         $(".feature .img").last().css("background-image","url(img/" + arr[i].img + ")");
         $(".feature .title").last().text(arr[i].title);
         $(".feature .date").last().text(arr[i].date);
-
         if ( 0 == i )
         {
             $(".feature").last().addClass("selected");
@@ -74,19 +72,12 @@ function displayArticles(arr)
 {
     var article = $(".article");
     $(".article").remove();
-    
-    article.clone().appendTo($("#article-list"));
-    $(".article .img").css("background-image","url(img/" + arr[0].img + ")");
-    $(".article .title").text(arr[0].title);
-    $(".article .synopsis p").text(arr[0].synopsis);
-    $(".article .date").text(arr[0].date);
-    /*var article = $(".article");
-    $(".article").remove();
     for ( var i in arr )
     {
         article.clone().appendTo($("#article-list"));
-        $(".article .img").last().css("background-image","url(img/" + arr[0].img + ")");
-        $(".article .title").last().text(arr[0].title);
-        $(".article .date").last().text(arr[0].date);
-        $(".article .synopsis p").last().text(arr[0].synopsis);*/
+        $(".article .img").css("background-image","url(img/" + arr[i].img + ")");
+        $(".article .title").text(arr[i].title);
+        $(".article .synopsis p").text(arr[i].synopsis);
+        $(".article .date").text(arr[i].date);
+    }
 }
